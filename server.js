8a13c1689eae41989e75bb245e0eb3cad97817c9
Daniel <daniel83fr@gmail.com>
2020-05-22 18:41:16 +0200

    fix build

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express')
+const path = require('path')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 require('dotenv').config()
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/GenealogyFrontEnd'));
+app.use(express.static(__dirname + '/dist/GenealogyFrontEnd'))
 
 app.get('/env', (req, res) => {
   res.send(
     {
-      "Environnement": process.env.NODE_ENV,
-      "GENEALOGY_API": process.env.GENEALOGY_API
+      Environnement: process.env.NODE_ENV,
+      GENEALOGY_API: process.env.GENEALOGY_API
     })
-});
+})
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/GenealogyFrontEnd/index.html'));
-});
+  res.sendFile(path.join(__dirname + '/dist/GenealogyFrontEnd/index.html'))
+})
 
-const documents = {};
+const documents = {}
 
-io.on("connection", socket => {
-  var clientIp = socket.request.connection.remoteAddress;
+io.on('connection', socket => {
+  var clientIp = socket.request.connection.remoteAddress
 
-  console.log('a user connected');
-  io.emit('message-received', "User connected: " + clientIp)
+  console.log('a user connected')
+  io.emit('message-received', 'User connected: ' + clientIp)
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
-    io.emit('message-received', "User disconnected: " + clientIp)
-  });
+    console.log('user disconnected')
+    io.emit('message-received', 'User disconnected: ' + clientIp)
+  })
 
   socket.on('message', (msg) => {
-    io.emit('message-received', msg);
-  });
+    io.emit('message-received', msg)
+  })
 
   socket.on('message-received', (msg) => {
-    console.log("message - " + msg)
-  });
-});
+    console.log('message - ' + msg)
+  })
+})
 
-let PORT = process.env.PORT
+const PORT = process.env.PORT
 http.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
-  console.log(`Env: ${process.env.NODE_ENV}`);
-});
+  console.log(`Server is running on port ${PORT}...`)
+  console.log(`Env: ${process.env.NODE_ENV}`)
+})
